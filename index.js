b6de655ba732fbd8bd72f777604e67d44baf6e1c
Melanie Alcivar <melanipau@live.com>
2025-09-10 19:30:44 -0500

    Actualizar archivos con cambios recientes

--- index.js
+++ index.js
@@ -1,27 +1,52 @@
 const express = require ('express');
-
+const { query, validationResult } = require ('express-validator');
 const app = express();
 
 app.use (express.json());
 
+
+
 const PORT = 3001;
 
 let users = [{
+    id: 1,
     name: 'Carlos',
+    email: 'carlos@gmail.com',
     age: 22
 }];   
 
-app.get('/',(req, res) => {
-    res.send('Hello World!!');
-});
 
-//Module users
-app.get('/users', (req, res) => {
+
+//Entity: users
+app.get('/api/users', (req, res) => {
     res.json({ code: 'OK', message: 'Users are available!', data: { users }});
-
      });
 
-app.post('/users', (req, res) => {
+
+
+app.get('/api/users/query', query('id').notEmpty(), (req, res) => {
+    
+    const errors = validationResult(req);
+
+    console.log ('Results:',result);
+    if (!errors.isEmpty()) {
+        return res.json({ code: 'PF', message: 'User ID is required!'});
+    }
+
+    const id = req.query.id;
+
+    const user = users.find ((user) => user.id == id);
+   
+    if (!user) {
+        return res.status(404).json({ code: 'NF', message: 'User not found!'});
+    }
+    res.json({ code: 'OK', message: 'Users are available!', data: { user }});
+});
+
+
+
+
+app.post('/api/users', (req, res) => {
     console.log ('POST /users:', req.body)
     const { name, age } = req.body;
     const newUser = { name,age };
@@ -29,18 +54,43 @@ app.post('/users', (req, res) => {
     res.json ({ code:  'OK', message: 'User created successfully!', data: { user: newUser }});
  });
 
-app.delete('/users/:name', (req, res) => {
-    const name = req.params.name;
-    console.log ('DELETE /users:', name);
-    const user = users.find (user => user.name == name);
+
+
+app.put('/api/users/:id', (req, res) => {
+    const id = req.params.id;
+    const user = users.find (user => user.id == id);
+
     if (user) {
-        users = users.filter (user => user.name != name);
+        /**Update user*/
+        const { name, email, age} = req.body;
+        user.name = name;
+        user.email = email;
+        user.age = age;
+
+        res.json({ code: 'OK', message: 'User updated successfully!', data: { user }});
+        return;
+    } 
+    /** User not found*/
+    res.json({ code: 'NF', message: 'User not found!'})
+});
+
+
+
+app.delete('/api/users/:id', (req, res) => {
+    const id = req.params.id;
+    console.log ('DELETE /users/:id', id);
+    const user = users.find (user => user.id == id);
+    if (user) {
+        users = users.filter (user => user.id != id);
         return res.json ({ code: 'OK', message: 'User deleted!', data: { user }})
 
     }
     res.json ({ code: "PF", message: 'User not found!' });
 
 });
+
+
+
 
 app.listen(PORT, () => {
     console.log (`Server is running on port ${PORT}`);
